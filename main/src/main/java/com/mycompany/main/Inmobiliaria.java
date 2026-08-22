8ced2ef4844b78b8106c787f2d707989385ef817
Bastian <Bastian@DESKTOP-F9I3JI7>
2026-08-22 18:15:41 -0400

    Creacion clase inmobiliaria 2

--- main/src/main/java/com/mycompany/main/Inmobiliaria.java
+++ main/src/main/java/com/mycompany/main/Inmobiliaria.java
@@ -1,6 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Inmobiliaria {
-    
+       
     private String nombre;
     private String direccion;
     private Map<String, Proyecto> proyectos;
@@ -10,23 +12,33 @@ public class Inmobiliaria {
         this.nombre = nombre;
         this.direccion = direccion;
         this.contacto = contacto;
-        this.proyecto = new Hasmap<>();
-    }
-    
-    
-    public String getNombre(){
-    return nombre;
+        this.proyecto = new HashMap<>();
     }
     
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
+    public String getNombre(){
+    return nombre;
+    }
+     
     public void setDireccion(String direccion){
         this.direccion = direccion;
     }
+    
+    public String getDireccion(){
+        return direccion;
+    }
+    
     public void setContacto(String contacto){
         this.contacto = contacto:
     }
-   
+    public String getContacto(){
+        return contacto;
+    }
+
+    public Map<String, Proyecto> getProyectos(){
+        return proyectos; 
+    }
     
 }
