cc60f74a062012ab2f41e07faa97a8e9cf481006
benja <>
2026-08-24 19:45:26 -0400

    codificación del main

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -1,6 +1,29 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Selecciona la forma de visualización");
+        System.out.println("1) Consola");
+        System.out.println("2) Ventana");
+        
+        int opcion = scanner.nextInt();
+        
+        switch (opcion){
+            case 1:
+                MenuConsola menu = new MenuConsola();
+                break;
+            case 2:
+                System.out.println("Abriendo Ventana.....");
+                break;
+            default:
+                System.out.println("Opción Inválida");
+
+        }
+        
+        scanner.close();
+        
         System.out.println("Hello World!");
         System.out.println("Prueba");
     }
