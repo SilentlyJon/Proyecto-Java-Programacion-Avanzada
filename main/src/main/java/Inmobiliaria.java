054c0c67c3cba368b6dad9bdb695f6d5f187ff7b
benja <rocio@LAPTOP-35JK2Q1N>
2026-08-25 19:18:56 -0400

    Se implementaron los métodos agregar, eliminar, mostrar, modificar y buscar proyectos en la clase.

--- main/src/main/java/Inmobiliaria.java
+++ main/src/main/java/Inmobiliaria.java
@@ -14,7 +14,33 @@ public class Inmobiliaria {
         this.contacto = contacto;
         this.proyecto = new HashMap<>();
     }
+    
+    public void agregarProyecto(){
+        proyecto.put(proyecto.getCodigo(),proyecto);
+    }
 
+    public Proyecto buscarProyecto(String codigo){
+        return proyecto.get(codigo);
+    }
+
+    public void modificarProyecto(String codigo, String nuevoNombre, String nuevaUbicacion, NivelDemanda nuevaDemanda){
+        Proyecto proyecto = proyecto.get(codigo);
+        
+        if(proyecto != null){
+            proyecto.setNombre(nuevoNombre);
+            proyecto.setUbicacion(nuevaUbicacion);
+            proyecto.setDemanda(nuevaDemanda);
+        }
+    }
+
+    public void eliminarProyecto(String codigo){
+        proyectos.remove(codigo);
+    }
+
+    public void mostrarProyecto(){
+        System.out.println(proyecto);
+    }
+   
     public String getNombre(){
         return nombre;
     }
@@ -36,10 +62,10 @@ public class Inmobiliaria {
     }
 
     public void setContacto(String contacto){
-        this.contacto = contacto
+        this.contacto = contacto;
     }
 
-    public Map<> getProyectos(){
+    public Map<String, Proyecto> getProyectos(){
         return proyectos;
     }
 
