83dd41c99cfab2762f21ea27d7816b214898c50d
benja <>
2026-08-25 19:22:54 -0400

    Se implemento un switch para llamar los metodos para la implementacion del menu en ventana y consola.

--- main/src/main/java/com/mycompany/main/Main.java
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -4,18 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+// EJEMPLO
+        Inmobilaria inmobilaria = new Inmobilaria("Inmobilaria Chile","Av. Principal","contaco@inmobilaria.cl");
+//
+
+        
         System.out.println("Selecciona la forma de visualización");
         System.out.println("1) Consola");
         System.out.println("2) Ventana");
-        
+
         int opcion = scanner.nextInt();
         
         switch (opcion){
             case 1:
-                MenuConsola menu = new MenuConsola();
+                MenuConsola menu = new MenuConsola(inmobilaria);
+                menu.mostrarMenu();
                 break;
             case 2:
                 System.out.println("Abriendo Ventana.....");
+                
                 break;
             default:
                 System.out.println("Opción Inválida");
@@ -26,5 +33,5 @@ public class Main {
         
         System.out.println("Hello World!");
         System.out.println("Prueba");
-    }
+   }
 }
