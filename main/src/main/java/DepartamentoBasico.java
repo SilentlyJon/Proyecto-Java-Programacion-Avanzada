157dc399df418e9faa3dbc3bf53a87f839217e0a
benja <>
2026-08-24 21:47:39 -0400

    Se creo clase proyecto y se corrigieron las clases DepartamentoBasico y Inmobiliaria.

---
+++ main/src/main/java/DepartamentoBasico.java
@@ -0,0 +1,10 @@
+public class DepartamentoBasico extends Departamento{
+    public DepartamentoBasico(String id, int numero, double metrosCuadrados, double precioBase, NivelDemanda demanda, EstadoDepartamento estado){
+        super(id, numero, metrosCuadrados, precioBase, demanda, estado);
+    }
+    
+    @Override
+    public double calcularPrecioFinal(){
+        return getPrecioBase(); 
+    }
+}
